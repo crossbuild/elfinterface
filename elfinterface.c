f857454e47db5824fd519837b366f452872134ce
Robert Schiele <rschiele@gmail.com>
2014-12-05 10:24:34 +0100

    add 64 bit support

--- elfinterface.c
+++ elfinterface.c
@@ -6,6 +6,20 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#ifdef E64
+typedef Elf64_Sym EI_Elf_Sym;
+typedef Elf64_Word EI_Elf_Word;
+typedef Elf64_Ehdr EI_Elf_Ehdr;
+typedef Elf64_Shdr EI_Elf_Shdr;
+#define EI_ELFCLASS ELFCLASS64
+#else
+typedef Elf32_Sym EI_Elf_Sym;
+typedef Elf32_Word EI_Elf_Word;
+typedef Elf32_Ehdr EI_Elf_Ehdr;
+typedef Elf32_Shdr EI_Elf_Shdr;
+#define EI_ELFCLASS ELFCLASS32
+#endif
+
 struct file {
     char* mem;
     off_t size;
@@ -33,33 +47,33 @@ static void closefile(struct file* f) {
     assert(res != -1);
 }
 
-static void parsesymbols(Elf32_Sym* symtab, Elf32_Word symnum) {
+static void parsesymbols(EI_Elf_Sym* symtab, EI_Elf_Word symnum) {
     unsigned int c;
     for (c = 1; c < symnum; ++c)
 	if ((symtab[c].st_shndx == SHN_UNDEF) &&
 	    (ELF32_ST_BIND(symtab[c].st_info) == STB_GLOBAL))
 	    symtab[c].st_info =
-		ELF64_ST_INFO(STB_WEAK, ELF32_ST_TYPE(symtab[c].st_info));
+		ELF32_ST_INFO(STB_WEAK, ELF32_ST_TYPE(symtab[c].st_info));
 }
 
-static void parseshdrs(void* mem, Elf32_Shdr* tab, int num) {
+static void parseshdrs(void* mem, EI_Elf_Shdr* tab, int num) {
     int c;
     for (c = 0; c < num; ++c)
 	if (tab[c].sh_type == SHT_DYNSYM) {
-	    assert(tab[c].sh_entsize == sizeof(Elf32_Sym));
-	    parsesymbols(mem + tab[c].sh_offset, tab[c].sh_size / sizeof(Elf32_Sym));
+	    assert(tab[c].sh_entsize == sizeof(EI_Elf_Sym));
+	    parsesymbols(mem + tab[c].sh_offset, tab[c].sh_size / sizeof(EI_Elf_Sym));
 	}
 }
 
 static void parseobject(void* mem) {
-    Elf32_Ehdr* hdr;
+    EI_Elf_Ehdr* hdr;
 
-    hdr = (Elf32_Ehdr*)(mem);
+    hdr = (EI_Elf_Ehdr*)(mem);
     assert(hdr->e_ident[EI_MAG0] == ELFMAG0);
     assert(hdr->e_ident[EI_MAG1] == ELFMAG1);
     assert(hdr->e_ident[EI_MAG2] == ELFMAG2);
     assert(hdr->e_ident[EI_MAG3] == ELFMAG3);
-    assert(hdr->e_ident[EI_CLASS] == ELFCLASS32);
+    assert(hdr->e_ident[EI_CLASS] == EI_ELFCLASS);
     assert(hdr->e_ident[EI_DATA] == ELFDATA2LSB);
     assert(hdr->e_ident[EI_VERSION] == EV_CURRENT);
     assert(hdr->e_ident[EI_OSABI] == ELFOSABI_NONE);
@@ -70,7 +84,7 @@ static void parseobject(void* mem) {
     assert(hdr->e_type == ET_DYN);
     /*assert(hdr->e_machine == EM_MIPS);*/
     assert(hdr->e_version == EV_CURRENT);
-    assert(hdr->e_shentsize == sizeof(Elf32_Shdr));
+    assert(hdr->e_shentsize == sizeof(EI_Elf_Shdr));
     parseshdrs(mem, mem + hdr->e_shoff, hdr->e_shnum);
 }
 
